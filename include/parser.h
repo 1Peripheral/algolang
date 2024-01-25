262bb6ceb8643a5f925e5a69b110159f7b0bdff0
1Peripheral <codels@protonmail.com>
2024-01-25 14:58:34 +0100

    Added utilitis for the parser

--- include/parser.h
+++ include/parser.h
@@ -1,4 +1,31 @@
 #pragma once
 
+#include "lexer.h"
+
 class Parser {
+public:
+   Parser(Lexer lexer);
+
+private:
+   void program();
+   void statement();
+   void comparison();
+   void expression();
+   void term();
+   void unary();
+   void primary();
+   void newLine();
+
+   void nextToken();
+   void match(TokenKind kind);
+   bool checkToken(TokenKind kind);
+   bool checkPeek(TokenKind kind);
+
+public:
+   Lexer lexer;
+   Token curToken;
+   Token peekToken;
+
+private:
+   Logger _logger;
 };
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,32 @@
 #include "../include/parser.h"
+
+Parser::Parser(Lexer lexer) : lexer(lexer) {
+   lexer.nextToken();
+   lexer.nextToken();
+}
+
+void Parser::program() {
+   std::cout << "PROGRAM" << std::endl;
+   while (!this->checkToken(ENDOF))
+      this->statement();
+}
+
+void Parser::nextToken() {
+   this->curToken = peekToken;
+   this->peekToken = this->lexer.nextToken();
+}
+
+void Parser::match(TokenKind kind) {
+   if (!this->checkToken(kind))
+      _logger.panic("Expected " + kindStringRepr.at(kind) + " got " + this->curToken.lexeme + " .");
+
+   this->nextToken();
+}
+
+bool Parser::checkToken(TokenKind kind) {
+   return this->curToken.kind == kind;
+}
+
+bool Parser::checkPeek(TokenKind kind) {
+   return this->peekToken.kind == kind;
+}
